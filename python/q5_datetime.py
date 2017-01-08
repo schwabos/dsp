a0a5095d8101f36368bf32a15f154f00def18c91
schwabos <epimoz@yahoo.com>
2017-01-08 14:07:17 -0800

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,41 @@
 # Hint:  use Google to find python function
 
 ####a) 
+# date_start = '01-02-2013'  
+# date_stop = '07-28-2015'  
+
+from datetime import datetime  
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'  
+date_format = '%m-%d-%Y'  
+start = datetime.strptime(date_start, date_format)  
+stop = datetime.strptime(date_stop, date_format)  
+total = stop - start  
+print(total.days)  
 
 ####b)  
+# date_start = '12312013'  
+# date_stop = '05282015' 
+
+from datetime import datetime  
 date_start = '12312013'  
 date_stop = '05282015'  
+date_format = '%m%d%Y'  
+start = datetime.strptime(date_start, date_format)  
+stop = datetime.strptime(date_stop, date_format)  
+total = stop - start  
+print(total.days) 
+
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+# date_start = '15-Jan-1994'  
+# date_stop = '14-Jul-2015' 
+
+from datetime import datetime  
+date_start = '15-Jan-1994'      
+date_stop = '14-Jul-2015'   
+date_format = '%d-%b-%Y'  
+start = datetime.strptime(date_start, date_format)  
+stop = datetime.strptime(date_stop, date_format)  
+total = stop - start  
+print(total.days) 
