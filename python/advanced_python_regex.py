58f61aa5e0dcda132b15db2d383204e030724826
schwabos <epimoz@yahoo.com>
2017-01-13 16:07:05 -0800

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+import pandas
+
+fac = pandas.read_csv('faculty.csv')
+
+# Q2
+import collections
+titles = []
+for x in range(len(fac)):
+ 	titles.append(fac.title[x])
+
+counter=collections.Counter(titles)
+print(counter)
+# Looking at the output, I assume there is a typo and 'is' should be 'of' for 'Assistant Progessor is Biostatistics'. 
+# Therefore final count: {'Professor of Biostatistics': 13, 'Associate Professor of Biostatistics': 12, 'Assistant Professor of Biostatistics': 12}
+
+# Q3
+emails = []
+for x in range(len(fac)):
+	emails.append(fac.email[x])
+print(emails)
